820613d9dffbfa2df79fe8b1975ce718c9b43c7f
dcnina <dcninaa@gmail.com>
2020-12-23 22:26:10 +0100

    changes pictures png

--- src/datas/photodata.js
+++ src/datas/photodata.js
@@ -1,14 +1,14 @@
 export default [
    {
     title: "de ma fenêtre",
-    photos: [  "/matinsuspendu.webp", "/fenetres1.webp", "/fenetres3.webp"]
+    photos: [  "/matinsuspendu.png", "/fenetres1.png", "/fenetres3.png"]
   },
   {
     title: "lignes directrices",
-    photos: ["/lignes.webp", "/toit.webp", "/lignes2.webp"]
+    photos: ["/lignes.png", "/toit.png", "/lignes2.png"]
   },
   {
     title: "furtivement vôtre",
-    photos: ["/furtivement.webp", "/furtivement2.webp", "/furtivement3.webp"]
+    photos: ["/furtivement.png", "/furtivement2.png", "/furtivement3.png"]
   }
 ];
